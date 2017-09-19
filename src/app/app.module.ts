4ae41e0c5109313a344e9905a4ae2d503709776a
Adrián Brito <adrbrpa1988@gmail.com>
2017-09-19 13:58:44 +0200

    Configure Wp-api-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { Http, HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { WordPressProvider } from '../providers/word-press/word-press';
+import { WpApiModule, WpApiLoader, WpApiStaticLoader} from 'wp-api-angular'
+
+export function WpApiLoaderFactory(http) {
+    return new WpApiStaticLoader(http, 'https://devdactic.com//wp-json/');
+}
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        WpApiModule.forRoot({
+            provide: WpApiLoader,
+            useFactory: (WpApiLoaderFactory),
+            deps: [Http]
+        }),
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        WordPressProvider
+    ]
 })
 export class AppModule {}
